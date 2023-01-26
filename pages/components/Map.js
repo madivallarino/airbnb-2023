847389a97dc9fa0382750b0dfcfded5e411f762d
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-26 17:24:12 -0500

    file switch

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,84 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { Map as ReactMapGL , Marker, Popup }  from 'react-map-gl';
-import { searchResults } from './searchResults';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { getCenter } from 'geolib';
+import React, { useState, useEffect } from "react";
+import { Map as ReactMapGL, Marker, Popup } from "react-map-gl";
+import { searchResults } from "../../searchResults";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { getCenter } from "geolib";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState({});
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
 
-    
-    const [ selectedLocation, setSelectedLocation ] = useState({})
-    const coordinates = searchResults.map((result) => ({
-        longitude: result.long,
-        latitude: result.lat
-    }));
+  // the center of the map
+  const center = getCenter(coordinates);
 
-    // the center of the map
-    const center = getCenter(coordinates)
-    
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
 
-    const [viewport, setViewport ] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: center.latitude,
-        longitude: center.longitude,
-        zoom: 11,
-    });
+  useEffect(() => {}, [selectedLocation]);
 
-  
-  
-
-    useEffect(() => {
-       
-    }, [selectedLocation]);
-    
-    
-    
-    
   return (
     <ReactMapGL
-    mapStyle="mapbox://styles/dirtyduck999/cldc2ybhj002601qrjwigek9v"
-    mapboxAccessToken={process.env.mapbox_key}
-    {...viewport}
-    onMove={evt => setViewport(evt.viewState)}
+      mapStyle="mapbox://styles/dirtyduck999/cldc2ybhj002601qrjwigek9v"
+      mapboxAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onMove={(evt) => setViewport(evt.viewState)}
     >
-
-        {searchResults.map(result => (
+      {searchResults.map((result) => (
         <div key={result.img}>
-            <Marker 
+          <Marker
             longitude={result.long}
             latitude={result.lat}
             offsetLeft={-20}
             offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
             >
-                <p 
-                role="img"
-                onClick={() => setSelectedLocation(result)} className='cursor-pointer text-2xl animate-bounce'
-                aria-label="push-pin">
-                    📌 
-                </p>
-            </Marker> 
-            
-            
-            {selectedLocation.long === result.long && (
-                <Popup 
-                  latitude={result.lat}
-                  longitude={result.long}
-                  onClose={() => setSelectedLocation({})}
-                  closeOnClick={false}
-                  >
-                    {result.title}
-                </Popup>
-            ) }
+              📌
+            </p>
+          </Marker>
 
+          {selectedLocation.long === result.long && (
+            <Popup
+              latitude={result.lat}
+              longitude={result.long}
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={false}
+            >
+              {result.title}
+            </Popup>
+          )}
         </div>
-        
-
-        ))}
-       
+      ))}
     </ReactMapGL>
-
-  )
+  );
 }
 
-export default Map
+export default Map;
--- pages/search.js
+++ pages/search.js
@@ -1,69 +1,64 @@
-import { useRouter } from 'next/router';
-import React from 'react'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import { format } from 'date-fns';
-import InfoCard from './components/InfoCard';
-import { searchResults } from './components/searchResults';
-import Map from './components/Map';
-
-
-
-
+import { useRouter } from "next/router";
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { format } from "date-fns";
+import InfoCard from "./components/InfoCard";
+import { searchResults } from "../searchResults";
+import Map from "./components/Map";
 
 function Search() {
-
-    const router = useRouter();
-    const { location, startDate, endDate, noOfGuests } = router.query;
-    const formattedStartDate = startDate && format(new Date(startDate), "dd MMMM yy");
-    const formattedEndDate = endDate && format(new Date(endDate), "dd MMMM yy");
-    const range = `${formattedStartDate} - ${formattedEndDate}`;
-    
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+  const formattedStartDate =
+    startDate && format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = endDate && format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
 
   return (
     <div>
-        <Header placeholder={`${location} | ${range} |  ${noOfGuests} guests`}/>
+      <Header placeholder={`${location} | ${range} |  ${noOfGuests} guests`} />
 
-        <main className='flex'>
-            <section className='flex-grow pt-14 px-6'>
-                <p className='text-xs'>300+ Stays - {range} - for {noOfGuests} number of guests</p>
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {noOfGuests} number of guests
+          </p>
 
-                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
 
-                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
-                    <p className='button'>Cancellation Flexibility</p>
-                    <p className='button'>Type of Place</p>
-                    <p className='button'>Prices</p>
-                    <p className='button'>Rooms and Beds</p>
-                    <p className='button'>More Filters</p>
-                </div>
-                <div className='flex flex-col'>
-                    {searchResults?.map(({ img, location, title, description, star, price, total}) => (
-                        <InfoCard 
-                        key={img}
-                        img={img}
-                        description={description}
-                        price={price}
-                        location={location}
-                        star={star}
-                        title={title}
-                        total={total}
-                     />
-                     ))}
-                </div>
-            
-            </section>
-            <section className='xl:inline-flex xl:min-w-[600px]'>
-                <Map />
-            </section>
-        </main>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Prices</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults?.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  description={description}
+                  price={price}
+                  location={location}
+                  star={star}
+                  title={title}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+        <section className="xl:inline-flex xl:min-w-[600px]">
+          <Map />
+        </section>
+      </main>
 
-        <Footer />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Search;
-
-
-
